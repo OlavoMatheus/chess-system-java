e39188bb1ef77ea9b83ae7a98ef65bcab0353ed6
Olavo Matheus <olavomatheus.faria@gmail.com>
2024-09-10 11:54:26 -0300

    Handling cptured pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -12,11 +14,13 @@ public class Program {
 
 	public static void main(String[] args) {
 		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
+		
 		Scanner sc = new  Scanner (System.in);
 		while(true){
 			try {
 				UI.clearScreen();
-				UI.printMatch(chessMatch);
+				UI.printMatch(chessMatch, captured);
 				System.out.println();
 				System.out.println("Source: ");
 				ChessPosition source= UI.readChessPosition(sc);
@@ -30,6 +34,9 @@ public class Program {
 				ChessPosition target= UI.readChessPosition(sc);
 				
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				if(capturedPiece!=null) {
+					captured.add(capturedPiece);
+				}
 			}
 			catch(ChessException e){
 				System.out.println(e.getMessage());
